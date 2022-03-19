8a24e74c7e40c230aa163c80cdb20fca1bc64dbc
nmsjeat <juho.koivisto@hotmail.com>
2022-03-19 13:45:13 +0200

    Added files

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-print('Test')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file1 = '../quote_20220318.csv'
+file2 = '../trade_20220318.csv'
+
+quote = pd.read_csv(file1, sep=',', index_col=0)
+trade = pd.read_csv(file2, sep=',', index_col=0)
+
+quote.index = pd.to_datetime(quote.index, format="%Y%m")
 
 
-print('This is a second edit')
-
-print('This a third edit')
+factors = factors.apply(lambda x: x.str.strip())
+factors = factors.apply(pd.to_numeric)
+factors
