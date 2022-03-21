bc4477932242f27c10b55342cc982a55b5dea4bb
nmsjeat <juho.koivisto@hotmail.com>
2022-03-21 11:42:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 
 # Read files
 file1 = '../quote_20220318.csv'
@@ -27,3 +28,32 @@ bch_trade = trade[trade['symbol'] == 'BCHUSD']
 xbt_quote = xbt_quote.assign(BuyPressure=lambda df: (df['bidSize'] / (df['bidSize'] + df['askSize'])).ewm(alpha=0.95).mean())
 eth_quote = eth_quote.assign(BuyPressure=lambda df: (df['bidSize'] / (df['bidSize'] + df['askSize'])).ewm(alpha=0.95).mean())
 bch_quote = bch_quote.assign(BuyPressure=lambda df: (df['bidSize'] / (df['bidSize'] + df['askSize'])).ewm(alpha=0.95).mean())
+
+# Weighted mid-price
+xbt_quote = xbt_quote.assign(WeightedMid=lambda df: df['BuyPressure'] * df['askPrice'] + (1 - df['BuyPressure']) * df['bidPrice'])
+eth_quote = eth_quote.assign(WeightedMid=lambda df: df['BuyPressure'] * df['askPrice'] + (1 - df['BuyPressure']) * df['bidPrice'])
+bch_quote = bch_quote.assign(WeightedMid=lambda df: df['BuyPressure'] * df['askPrice'] + (1 - df['BuyPressure']) * df['bidPrice'])
+
+# Micro price adjustment
+xbt_quote = xbt_quote.assign(MicroPriceAdjustment=lambda df: df['WeightedMid'] - 0.5 * (df['askPrice'] + df['bidPrice']))
+eth_quote = eth_quote.assign(MicroPriceAdjustment=lambda df: df['WeightedMid'] - 0.5 * (df['askPrice'] + df['bidPrice']))
+bch_quote = bch_quote.assign(MicroPriceAdjustment=lambda df: df['WeightedMid'] - 0.5 * (df['askPrice'] + df['bidPrice']))
+
+
+
+
+
+
+
+
+y = bch_quote.resample('1s').first().dropna()['bidPrice']
+y = (y.shift(1) - y)[1:]
+x = bch_quote.resample('1s').first().dropna()['MicroPriceAdjustment'][1:]
+x[x.between(-0.05,0.05)] = 0
+x = sm.add_constant(x, has_constant='add')
+model = sm.OLS(y,x).fit()
+model.summary()
+
+
+
+
